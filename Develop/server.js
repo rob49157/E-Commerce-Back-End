b89e213593850ff6502202fa20a13a032526eca0
rob49157 <robertopaipo@hotmail.com>
2021-11-20 15:26:29 -0500

    syncing tables

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const routes = require('./routes');
+import { sequelize } from './models/Product';
+import * as Sequelize from '/config/connection.js';
+
 // import sequelize connection
 
 const app = express();
@@ -12,5 +15,6 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
+  await sequelize.sync({force: true})
   console.log(`App listening on port ${PORT}!`);
 });
