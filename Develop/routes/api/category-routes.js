a6edbae2fd6a5ef6aa63ca87c31ea59b4640b91a
rob49157 <robertopaipo@hotmail.com>
2021-11-21 11:03:08 -0500

    findpk catergories

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -5,13 +5,21 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  const findall =
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  const findpk=   await findpk.findbypk(id)
+    if (findpk=== null){
+      console.group('not found')
+    } else{ 
+      console.log (findpk)
+    }
+  }
   // be sure to include its associated Products
-});
+);
 
 router.post('/', (req, res) => {
   // create a new category
